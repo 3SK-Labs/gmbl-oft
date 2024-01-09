929945cd6b9a9c50e7254624887be854ab58c560
Roy Blankman <blankman@boustro.com>
2024-01-09 16:48:53 +0100

    configuring networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config()
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.22",
   networks: {
-    hardhat: {
-      forking: {
-        url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
-        blockNumber: 16172816,
-      },
+    arbitrum: {
+      url: `${process.env.ARBITRUM_RPC}`,
+      accounts: [`${process.env.DEPLOYER_PK}`]
+    },
+    avalanche: {
+      url: `${process.env.AVAX_RPC}`,
+      accounts: [`${process.env.DEPLOYER_PK}`]
     }
   }
 };
