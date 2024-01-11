6474fdbf384c8529e5e5515d9f28b0c7d4efb6c4
Roy Blankman <blankman@boustro.com>
2024-01-11 12:28:19 +0100

    verifying contract on arbitrum

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ LayerZero OFT contracts for:
 
 1. wrapping original GMBL token on arbitrum in a ProxyOFT
 2. deploying an GMBL OFT contract to AVAX so the GMBL token can be bridged their
+
+## arbitrum verification
+
+```bash
+$ npx hardhat verify --constructor-args constants/arb_arguments.js --contract "contracts/GmblTokenProxyOFT.sol:GmblTokenProxyOFT" --network arbitrum 0x236626887687bf6a102269207f8f0A08549Ae4f4
+```
--- hardhat.config.js
+++ hardhat.config.js
@@ -20,5 +20,23 @@ module.exports = {
       url: `${process.env.AVAX_RPC}`,
       accounts: [`${process.env.DEPLOYER_PK}`]
     }
+  },
+  etherscan: {
+      apiKey: {
+          arbitrum: `${process.env.ARBISCAN_API_KEY}`,
+      },
+      customChains: [
+          {
+              network: "arbitrum",
+              chainId: 42161,
+              urls: {
+                  apiURL: "https://api.arbiscan.io/api",
+                  browserURL: "https:/arbiscan.io/",
+              },
+          },
+      ],
+  },
+  sourcify: {
+      enabled: true,
   }
 };
