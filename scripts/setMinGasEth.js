df69cc6475a3139b8d14259825d9d8ffa46383e8
Roy Blankman <blankman@boustro.com>
2024-02-08 19:55:07 +0100

    setting gas on eth OFT

--- scripts/setMinGasEth.js
+++ scripts/setMinGasEth.js
@@ -2,9 +2,9 @@ const hre = require("hardhat")
 const CHAIN_ID = require("../constants/chainIds.json")
 
 async function main() {
-    const address = '0x236626887687bf6a102269207f8f0A08549Ae4f4'
-    const contract = await hre.ethers.getContractAt("GmblTokenProxyOFT", address);
-    const dstChainId = CHAIN_ID['ethereum']
+    const address = '0xFbf1a6a5b0758Db15A7AaAe403DbbE7A9cc8670A'
+    const contract = await hre.ethers.getContractAt("GmblTokenOFT", address);
+    const dstChainId = CHAIN_ID['arbitrum']
     const tx = await contract.setMinDstGas(dstChainId, 0, 100000)
 
     console.log(`[${hre.network.name}] setMinDstGas tx hash ${tx.hash}`)
